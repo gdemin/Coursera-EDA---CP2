18267d2d41719ba8ab00c3ecc99b504dc3bb22d3
Gregory Demin <gdemin+github@gmail.com>
2014-10-24 00:06:19 +0400

    Almost finished plot1

--- Plot1.R
+++ Plot1.R
@@ -6,6 +6,7 @@
 # you should change path according to ypur system
 setwd("C:/Users/gregory/Documents/!Projects/Trainings/Coursera - Exploratory Data Analysis/Coursera-EDA---CP2")
 
+library(dplyr)
 options(stringsAsFactors = FALSE)
 # Data was downloaded from https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
 # on 23.10.2014 with commented line below. It is not reasonable to download data every time so it is commented.
@@ -15,6 +16,23 @@ options(stringsAsFactors = FALSE)
 unzip("eda_cp2_data.zip")
 
 # read data
-power_cons_data = read.table(unzipped_file,header=TRUE,sep=";",na.strings = "?",stringsAsFactors = FALSE)
+NEI = readRDS("summarySCC_PM25.rds")
+SCC = readRDS("Source_Classification_Code.rds")
 
-unlink (unzipped_file)
+# we will keep only zipped version
+unlink("Source_Classification_Code.rds")
+unlink("summarySCC_PM25.rds")
+
+
+total_per_year = NEI %>% group_by(year) %>% summarize(Emissions = sum(Emissions))
+par(xaxs = "i")
+with(total_per_year,plot(year, Emissions/1e6,
+                         xlim = c(1999,2008),
+                         type = "b",
+                         main = expression(paste("USA total emission of ",PM[2.5],
+                                                 " was decreased during 1999-2008")),
+                         xlab = "Year",
+                         ylab = expression(paste(PM[2.5], ", million tons"))))
+
+title(sub= "Pollution siginificantly decrease.")
+
